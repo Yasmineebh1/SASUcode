f222a0ae4b10a0afc1abdfd13a1fa12bd88419c1
Yasmineebh1 <bahiyasmine.13@gmail.com>
2024-09-26 11:27:12 +0200

    factoriel

--- C/day01/boucles/challenge2/challenge2.c
+++ C/day01/boucles/challenge2/challenge2.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 //3!=3*2*1
 int main(){
-    float f, n;
+    int f, n;
     printf("entrer un nombre: ");
     scanf("%d",&n);
 
-    if(n == 0)
+    if(n <= 0)
         return 1;
     else{
+        f = 1;
         for(int i = 1;i <= n; i++){
-            f = f * n;
+            f = f * i;
         }
         printf("la factoriel de %d est %d",n,f);
     }
