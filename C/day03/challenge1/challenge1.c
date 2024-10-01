6941d97f049af9874a516a1d658828cd82939036
Yasmineebh1 <bahiyasmine.13@gmail.com>
2024-10-01 17:40:23 +0200

    challenge1

---
+++ C/day03/challenge1/challenge1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char c[100];
+
+    printf("entrer une charactere: ");
+    gets(c);
+    puts(c);
+
+    return 0;
+
+    // printf("la chaine de caractere que vous creez est: ",c);
+}
